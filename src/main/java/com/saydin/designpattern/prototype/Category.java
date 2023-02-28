d4e870a3dac3d5b31e0c601a43d17fca1a89d626
Semiha Aydın <semiha.aydinn@gmail.com>
2023-02-28 14:32:44 +0300

    prototyple pattern example

---
+++ src/main/java/com/saydin/designpattern/prototype/Category.java
@@ -0,0 +1,38 @@
+package com.saydin.designpattern.prototype;
+
+public class Category implements Cloneable{
+
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Category clone() throws CloneNotSupportedException {
+        return (Category) super.clone();
+    }
+
+}
